b2ce1d8e3cee65c1e8f9c6beff31553fefd2feed
Ebrahim Afridi <afridiebrahimck@gmail.com>
2024-02-16 17:39:07 +0530

    added: custom hook useAppSupplies

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,10 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-//
-// function App() {
-//   const [count, setCount] = useState(0)
-//
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-//
-// export default App
-
-
 import TransactionTable from "../components/TransactionTableStyled.tsx";
 import ChainSelectorComponent from "../components/ChainSelector.tsx";
 import AppToast from "../ui/AppToast.tsx";
 import BurnStatsContainer from "../components/BurnStatsContainer.tsx";
 import BurnButtonBar from "../components/BurnButtonBar.tsx";
+import {useEffect} from "react";
+import useAppSupplies from "./hooks/useAppSupplies.tsx";
 
 const BurnPageStyled = styled.div``;
 
@@ -56,11 +21,12 @@ const App = () => {
         isBalanceError,
         openChainModal,
         walletChain,
-        chains,
+        chains: receiveChains,
         openConnectModal,
     } = useWallet();
+
     const {openChainSelector, setOpenChainSelector, openChainSelectorModal} = useChainSelector();
-    const {chains: receiveChains} = useWallet();
+
     const {
         supplies,
         allSupplies,
@@ -68,6 +34,7 @@ const App = () => {
         suppliesChain,
         fetchSupplies,
     } = useAppSupplies(true);
+
     const [burnTransactions, setBurnTransactions] = useState<any[]>([]);
     const [isOldToken, setIsOldToken] = useState(false);
     const [burnAmount, setBurnAmount] = useState("");
